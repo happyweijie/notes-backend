e41a99d376f09c9d2c524cdf7b06bfa83a50664e
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-06-07 22:49:13 +0800

    Add update and delete endpoints

--- index.js
+++ index.js
@@ -24,10 +24,15 @@ app.get('/api/notes', (request, response) => {
   });
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id).then(note => {
-    response.json(note)
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -49,11 +54,30 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter((note) => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+  
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        response.status(401).end()
+      } 
+
+      note.content = content
+      note.important = important
+
+      return note.save()
+        .then((updatedNote) => response.json(updatedNote))
+    })
+    .catch(error => next(error))
 })
 
 // middleware for handling requests with unknown endpoint
@@ -61,7 +85,19 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+// middleware for malformatted data
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
