7049d6b47522bdfd54a360747c1336260f99a348
Chew Wei Jie <95748108+happyweijie@users.noreply.github.com>
2026-06-12 22:27:24 +0800

    Add validation for content for notes

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/api/notes/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -49,9 +49,9 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   })
 
-  note.save().then((savedNote) => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then((savedNote) => response.json(savedNote))
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
@@ -91,7 +91,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
 
   next(error)
 }
